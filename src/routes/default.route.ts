5459b8b2b53ba8b4e2aa8e6c180567fe77285f18
Sriram Hariharan <sghsri@gmail.com>
2025-04-17 00:17:28 -0500

    refactor: streamline ESLint and Prettier configurations, remove unused template route and tests, enhance default route with Zod validation for parameters and response

--- src/routes/default.route.ts
+++ src/routes/default.route.ts
@@ -1,11 +1,33 @@
 import { rateLimiter } from "src/middlewares/rateLimiter.middleware";
 import { RouteConfig } from "src/lib/routeLoader";
 import { createHandler } from "src/lib/createHandler";
+import { z } from "zod";
 
-const handler = createHandler({}, async (_, res) =>
-    res.status(200).json({
-        message: `${process.env.npm_package_name} connected @ ${new Date().toISOString()}`,
-    })
+const handler = createHandler(
+    {
+        params: z.object({
+            id: z
+                .string()
+                .min(20)
+                .optional()
+                .describe("The id of the user (optional) This is just an example paramter"),
+        }),
+        response: z.object({
+            message: z
+                .string()
+                .optional()
+                .describe("The message of the user (optional) This is just an example response"),
+        }),
+    },
+    async (req, res) => {
+        const { id } = req.params;
+
+        res.status(200).json({
+            message: `${process.env.npm_package_name} connected @ ${new Date().toISOString()}${
+                id ? ` with id ${id}` : ""
+            }`,
+        });
+    }
 );
 
 export const route: RouteConfig = {
